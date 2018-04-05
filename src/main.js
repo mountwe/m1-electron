64870580d37cae8377be560f817ff50636f88aee
Gene Mountjoy <mountwe@live.com>
2018-04-05 13:37:36 -0400

    Add functionality for 'Close Window' button to actually close the window.

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ const electron = require('electron')
 const remote = electron .remote
 
 document.getElementById("close-button").addEventListener("click", function(e) {
-    alert("Close button pressed")
+    // alert("Close button pressed")
+    const window = remote.getCurrentWindow()
+    window.close()
 })
